e01a5668e167269bae304ae10cc2d510b39d0ce5
LnData <96571968+LnDataTech@users.noreply.github.com>
2024-11-11 17:58:45 +0800

    beta 1.5

--- js/Section_1_3_hover.js
+++ js/Section_1_3_hover.js
@@ -2,14 +2,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const peopleHoverIcon = document.querySelector('.peopleHoverIcon');
     const contentBox = document.querySelector('.content_box9');
+    const quoteText = document.querySelector('.quoteText');
 
     // 當滑鼠滑過色塊時
     peopleHoverIcon.addEventListener('mouseenter', function () {
-        contentBox.style.opacity = '100%'; // 顯示內容區域
+        quoteText.innerHTML = '（躺平人生似乎也是個好選擇...）';
     });
 
     // 當滑鼠移開色塊時
     peopleHoverIcon.addEventListener('mouseleave', function () {
-        contentBox.style.opacity = '0'; // 隱藏內容區域
+        quoteText.innerHTML = '當主管後才發現什麼都要管，反而更常加班!';
     });
 });
